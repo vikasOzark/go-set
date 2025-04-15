ec4eceddce71bd18f9e06a67355ab7c485724bd3
Vikas <vikask99588@gmail.com>
2025-04-15 11:15:15 +0530

    Added set data structure implimentation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vikasOzark/go-set
+
+go 1.24.2
---
+++ set.go
@@ -0,0 +1,34 @@
+package goset
+
+type Set[T comparable] struct {
+	data map[T]struct{}
+}
+
+func NewSet[T comparable]() *Set[T] {
+	return &Set[T]{data: make(map[T]struct{})}
+}
+
+func (s *Set[T]) Add(value T) {
+	s.data[value] = struct{}{}
+}
+
+func (s *Set[T]) Remove(value T) {
+	delete(s.data, value)
+}
+
+func (s *Set[T]) In(value T) bool {
+	_, exists := s.data[value]
+	return exists
+}
+
+func (s *Set[T]) Values() []T {
+	keys := make([]T, 0, len(s.data))
+	for key := range s.data {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (s *Set[T]) Size() int {
+	return len(s.data)
+}
